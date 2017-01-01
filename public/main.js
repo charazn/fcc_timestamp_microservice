3ffb520785ec489494499aa7724bfbc646ce5f03
Char Aznable <charazn37@gmail.com>
2017-01-01 20:40:14 +0800

    Add main.js file for index.html

---
+++ public/main.js
@@ -0,0 +1,7 @@
+// var moment = require('moment');
+
+// document.getElementById("dateNow").innerHTML = moment.format('Do MMMM YYYY, h:mm:ss a');
+// var moment = require('moment');
+
+// var today = new Date();
+document.getElementById("dateNow").innerHTML = new Date();
